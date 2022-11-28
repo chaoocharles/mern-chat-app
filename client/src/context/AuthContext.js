3e6433e7e9b9401743623787fcc9237e3bab52dd
Charles Kairu <chaoocharles@gmail.com>
2022-11-29 01:25:16 +0300

    removed ts

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  console.log("Auth State", user);
+
+  const registerUser = () => {};
+
+  const loginUser = () => {};
+
+  const logoutUser = () => {};
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        registerUser,
+        loginUser,
+        logoutUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
