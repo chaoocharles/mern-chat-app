7453fe50befa256a10e9be07bc92d5304eefaaca
Charles Kairu <chaoocharles@gmail.com>
2022-11-28 19:18:22 +0300

    fix register request

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -3,7 +3,7 @@ const { registerUser, loginUser } = require("../Controllers/userController");
 
 const router = express.Router();
 
-router.get("/register", registerUser);
+router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 module.exports = router;
