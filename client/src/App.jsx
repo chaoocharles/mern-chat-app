e31d0446ea3cd78f5ba0697056131b235c58aea5
Charles Kairu <chaoocharles@gmail.com>
2022-11-29 14:33:54 +0300

    register form/state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Chat from "./components/Chat";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Chat />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,13 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [registerInfo, setRegisterInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
   console.log("Auth State", user);
+  console.log("Register State", registerInfo);
 
-  // use usecallback
+  const updateRegisterInfo = useCallback((info) => {
+    setRegisterInfo(info);
+  }, []);
 
   const registerUser = () => {};
 
@@ -22,6 +30,8 @@ export const AuthContextProvider = ({ children }) => {
         registerUser,
         loginUser,
         logoutUser,
+        registerInfo,
+        updateRegisterInfo,
       }}
     >
       {children}
