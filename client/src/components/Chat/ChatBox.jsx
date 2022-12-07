ca0bb177b76249f0c631479de1fe2469980e493f
Charles Kairu <chaoocharles@gmail.com>
2022-12-07 09:55:04 +0300

    getMessages

--- client/src/components/Chat/ChatBox.jsx
+++ client/src/components/Chat/ChatBox.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+import { baseUrl, getRequest } from "../../utils/service";
 
 const ChatBox = () => {
   const { user } = useContext(AuthContext);
   const { currentChat } = useContext(ChatContext);
   const { recipientUser } = useFetchRecipientUser(currentChat, user);
 
-  console.log("recipientUser", recipientUser);
+  const [messages, setMessages] = useState(null);
+  const [messagesError, setMessagesError] = useState(null);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      const response = await getRequest(
+        `${baseUrl}/messages/${currentChat?._id}`
+      );
+
+      if (response.error) {
+        return setMessagesError(error);
+      }
+
+      setMessages(response);
+    };
+    getMessages();
+  }, [currentChat]);
+
+  console.log("messages", messages);
+  console.log("messagesError", messagesError);
 
   return <div>{recipientUser?.name}</div>;
 };
