2da12b10df64304f62199c448c0e3b8dbff1050b
Charles Kairu <chaoocharles@gmail.com>
2022-12-10 18:14:12 +0300

    get all users api

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -74,4 +74,14 @@ const findUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser, findUser };
+const getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { registerUser, loginUser, findUser, getUsers };
--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -3,6 +3,7 @@ const {
   registerUser,
   loginUser,
   findUser,
+  getUsers,
 } = require("../Controllers/userController");
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/find/:userId", findUser);
+router.get("/users", getUsers);
 
 module.exports = router;
